9ae6deaa66470ed34debb781c089c533b09ea531
pirocchi <pirocchi_hiromu@mac.com>
2026-05-24 19:26:16 +0900

    fix: adjust use-client directive order and force dynamic rendering

--- src/app/tokutan/page.tsx
+++ src/app/tokutan/page.tsx
@@ -1,7 +1,6 @@
-export const dynamic = "force-dynamic";
-
 "use client";
 
+export const dynamic = "force-dynamic";
 import React, { useState, useEffect, useMemo, useContext } from "react";
 import { ThemeContext } from "../layout";
 import { 
